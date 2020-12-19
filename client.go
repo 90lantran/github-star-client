91146e1092c98eb410b1532a4ed4ca3626337351
90lantran <lantran@terpmail.umd.edu>
2020-12-19 17:58:03 -0500

    added unit-test

--- client.go
+++ client.go
@@ -19,26 +19,28 @@ type Request struct {
 }
 
 type Response struct {
-	TotalStars   int64            `json:"totalStars,omitempty"`
-	InvalidRepos []string         `json:"invalidRepos,omitempty"`
-	ValidRepos   map[string]int64 `json:"validRepos,omitempty"`
-	Error        string           `json:"error,omitempty"`
-	Status       string           `json:"status" validate:"required"`
+	TotalStars   int64         `json:"totalStars,omitempty"`
+	InvalidRepos []string      `json:"invalidRepos,omitempty"`
+	ValidRepos   []MapNameStar `json:"validRepos,omitempty"`
+	Error        string        `json:"error,omitempty"`
+	Status       string        `json:"status" validate:"required"`
+}
+
+type MapNameStar struct {
+	Name string `json:"name"`
+	Star int64  `json:"star(s)"`
 }
 
 func validateListInput(list []string) error {
 	var validInput = regexp.MustCompile(`^[a-zA-Z0-9\_\-\.]+\/[a-zA-Z0-9\_\-\.]+$`)
-
 	for _, l := range list {
 		elements := strings.Split(l, ",")
 		for _, e := range elements {
-			fmt.Println("e: " + strings.TrimSpace(e))
 			if !validInput.MatchString(strings.TrimSpace(e)) {
 				return fmt.Errorf("-r input list %s is not valid. Valid format is list of organization/repository", e)
 			}
 		}
 	}
-
 	return nil
 }
 
@@ -49,44 +51,40 @@ func validateHostAndPort(hosts []string) error {
 			return fmt.Errorf("-t Host and Port %s are not valid. Valid format ip:port", host)
 		}
 	}
-
 	return nil
 }
 
-func main() {
+var argList *[]string
+var argHost *string
+var parser *argparse.Parser
 
-	//go run main.go  -r golang/go -r tinygo-org/tinygo-site -r golang/g
-	// Create new parser object
-	parser := argparse.NewParser("client", "sends a request and receives a response from github-star server")
+func init() {
+	//create create parser
+	parser = argparse.NewParser("client", "sends a request and receives a response from github-star server")
 
-	argList := parser.StringList("r", "request",
+	argList = parser.StringList("r", "request",
 		&argparse.Options{
 			Required: true,
 			Help:     "List of organization/repossitory to send to server.",
 			Validate: validateListInput})
 
-	argHost := parser.String("t", "host",
+	argHost = parser.String("t", "host",
 		&argparse.Options{
 			Required: false,
 			Help:     "IP address and port of the server.",
 			Validate: validateHostAndPort,
 			Default:  "localhost:8080"})
-	// Create string flag
-	//s := parser.String("r", "request", &argparse.Options{Required: true, Help: "Json Payload to send to server"})
+}
 
-	// Parse input
+func main() {
+	// parse input
 	err := parser.Parse(os.Args)
 	if err != nil {
-		// In case of error print error and print usage
-		// This can also be done by passing -h or --help flags
 		fmt.Print(parser.Usage(err))
 		return
 	}
 
-	fmt.Println("argHost: " + *argHost)
-
-	// Finally print the collected string
-	//fmt.Println(*s)
+	// create list of orginization/repository
 	completeList := []string{}
 	for _, arg := range *argList {
 		elements := strings.Split(arg, ",")
@@ -96,32 +94,29 @@ func main() {
 	request := Request{
 		Input: completeList,
 	}
-	fmt.Printf("v: %+v\n", request)
+	fmt.Printf("input list: %+v\n", request)
 
 	jsonStr, _ := json.Marshal(request)
-
-	fmt.Println(fmt.Sprintf("%s", jsonStr))
 
 	req, err := http.NewRequest("POST", "http://"+*argHost+"/get-stars", bytes.NewBuffer(jsonStr))
 	if err != nil {
 		log.Fatalf("cannot create a POST request %v\n", err)
 	}
-	// defer resp.Body.Close()
 	req.Header.Set("Content-Type", "application/json")
 
+	// create default http client
 	client := http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("client cannot send request %v", err)
+		log.Fatalf("client cannot send request %v\n", err)
 		return
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-
 	var response Response
 	err = json.Unmarshal(body, &response)
-
-	fmt.Printf("Response: %+v\n", response)
-
+	// pretty print json response
+	responseJSON, _ := json.MarshalIndent(response, "", "  ")
+	fmt.Printf("Response: %+v\n", string(responseJSON))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/90lantran/github-star-client
 
 go 1.15
 
-require github.com/akamensky/argparse v1.2.2
+require (
+	github.com/akamensky/argparse v1.2.2
+	github.com/smartystreets/goconvey v1.6.4
+)
